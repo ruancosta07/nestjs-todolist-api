9273c56678bbb4adde7b32acbb1d68e9203dc068
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-01-29 16:05:28 -0300

    feat: servidor de produção na documentação

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder().setTitle("Documentação API todolist").setDescription("Documentação de uma api simples de gerenciamento de tarefas com NestJS").setVersion("1.0").addServer("http://localhost:3000").addBearerAuth().build()
+  const config = new DocumentBuilder().setTitle("Documentação API todolist").setDescription("Documentação de uma api simples de gerenciamento de tarefas com NestJS").setVersion("1.0").addServer("http://localhost:3000").addBearerAuth().addServer("https://nestjs-todolist-api.onrender.com/").build()
   const documentFactory = () => SwaggerModule.createDocument(app, config)
   SwaggerModule.setup("api", app, documentFactory)
   await app.listen(process.env.PORT ?? 3000);
